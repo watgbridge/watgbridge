07f9f68bbdda79b431eb90eb93582b3868125bd5
Akshett Rai Jindal <jindalakshett@gmail.com>
2024-06-29 14:30:51 +0530

    flake: keep only dev packages, rest will be moved to NUR

--- flake.nix
+++ flake.nix
@@ -34,13 +34,11 @@
         };
 
         packages = rec {
-          watgbridge = (pkgs.callPackage ./nix/pkgs/watgbridge.nix {});
-          # # vX.Y.Z for watgbridge-dev means git after watgbridge-vX.Y.Z release
-          watgbridge-dev = (pkgs.callPackage ./nix/pkgs/watgbridge-dev.nix {});
+          # # vX.Y.Z for watgbridge means git after watgbridge-vX.Y.Z release
+          watgbridge = (pkgs.callPackage ./nix/pkgs/watgbridge-dev.nix {});
 
-          watgbridgec2c = (pkgs.callPackage ./nix/pkgs/watgbridgec2c.nix {});
-          # # vX.Y.Z for watgbridgec2c-dev means git after watgbridgec2c-vX.Y.Z release
-          watgbridgec2c-dev = (pkgs.callPackage ./nix/pkgs/watgbridgec2c-dev.nix {});
+          # # vX.Y.Z for watgbridgec2c means git after watgbridgec2c-vX.Y.Z release
+          watgbridgec2c = (pkgs.callPackage ./nix/pkgs/watgbridgec2c-dev.nix {});
 
           default = watgbridge;
         };
